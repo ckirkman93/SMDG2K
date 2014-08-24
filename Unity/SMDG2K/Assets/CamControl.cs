8f570149877c8363dd708ea89b0f3664f366dd82
zeroxiii <rlodhia@Rahmaan-Lodhias-Macbook.local>
2014-08-24 16:23:11 -0400

    First Prototype
    *Moving paddle
    *Camera shift

---
+++ Unity/SMDG2K/Assets/CamControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+	public GameObject pivot;
+	private float ymin = 11.73f, ymax = 12.43f, zmin = 8.55f, zmax = 6.70f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pivot.transform.rotation.eulerAngles.z < 325.0f && pivot.transform.rotation.eulerAngles.z > 45.0f) {
+				this.camera.orthographicSize = zmin;
+				transform.position = new Vector3(0f, ymin, -10f);
+			///Debug.Log("small");
+			} 
+		else {
+				this.camera.orthographicSize = zmax;
+				transform.position = new Vector3(0f, ymax, -10f);
+			//Debug.Log("large");
+		}
+	}
+}
---
+++ Unity/SMDG2K/Assets/PaddleControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dir = Input.GetAxis ("Horizontal") * 1.5f;
+		transform.Rotate(new Vector3(0f, 0f, -dir));
+	}
+}
